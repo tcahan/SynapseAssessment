dcf3d7c6fb235870f8d53c8aa1e6a61a7fc80001
Timothy Cahan <timothy@cahan.dev>
2024-08-28 12:25:12 -0500

    Enhancement #3 - Add configuration changes
    Added the appsettings.json file. Included the necessary Microsoft.Extensions.Hosting NuGet package. Configured the program.cs file to use the new appsettings and allow reading from the configuration.

--- SynapseAssessmentApp/SynapseAssessment/Program.cs
+++ SynapseAssessmentApp/SynapseAssessment/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+var builder = new ConfigurationBuilder();
+BuildConfig(builder);
+IConfiguration configuration = builder.Build();
+
+// Test code to be removed later
+Console.WriteLine($"Reading Test config: {configuration.GetValue<string>("Test")}");
+
+/// <summary>
+/// Configures the configuration builder to support
+/// reading configuration from appsettings.json, appsettings.{Environment}.json
+/// </summary>
+/// <param name="builder">The builder object to be configured by reference</param>
+static void BuildConfig(IConfigurationBuilder builder)
+{
+	builder.SetBasePath(Directory.GetCurrentDirectory())
+		.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+		.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+		.AddEnvironmentVariables();
+}
